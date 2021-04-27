b5a34dd21e46d9335a0313d3ce84bf2325adde5a
Leonid Lypovyk <leonavr99@gmail.com>
2021-04-27 14:27:20 +0300

    + save

--- cmd_test.py
+++ cmd_test.py
@@ -3,7 +3,6 @@ def calc():
     if sys.argv[3] =='+':
         res = int(sys.argv[1]) + int(sys.argv[2])
         return res
-
     elif sys.argv[3] =='-':
         res = int(sys.argv[1]) - int(sys.argv[2])
         return res
@@ -15,14 +14,21 @@ def calc():
     elif sys.argv[3] =='/':
         res = int(sys.argv[1]) / int(sys.argv[2])
         return res
-with open("log.txt", "w") as f:
-    f.write(f"{sys.argv[1]} {sys.argv[3]} {sys.argv[2]} = {calc()}")
+
+def write(res):
+    if '-f' in sys.argv[1:]:
+        with open(f"{sys.argv[2]}", "w") as f:
+            f.write(res)
+            print(res)
+
 
 if '-h' in sys.argv[1:]:
-    print ("Это калькулятор, введите название файла, 1-ю цифру, через пробел 2-ю цифру и операнд.\n'-v в конце' - для вывода хода операции")
+    print ("Это калькулятор, введите название файла, 1-ю цифру, через пробел 2-ю цифру и операнд.\n'-v в конце' - для вывода хода операции\n'-f в конце и имя файла' - для сохранения результата")
 
 elif '-v' in sys.argv[1:]:
     print (f"{sys.argv[1]} {sys.argv[3]} {sys.argv[2]} = {calc()}")
-
+elif '-f' in sys.argv[1:]:
+        with open(f"{sys.argv[5]}", "w") as f:
+            f.write(f"{sys.argv[1]} {sys.argv[3]} {sys.argv[2]} = {calc()}")
 else: 
     print(calc())
