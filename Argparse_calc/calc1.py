7494f666a62b5f6842218f4a0530ab8bcf88045a
Leonid Lypovyk <leonavr99@gmail.com>
2021-05-04 15:41:31 +0300

    Homework 05.05.21

---
+++ Argparse_calc/calc1.py
@@ -0,0 +1,69 @@
+import argparse
+parser = argparse.ArgumentParser()
+nums = parser.add_argument('nums', type=int, nargs='+',
+                    help='Список всех введенных цифр')
+oper = parser.add_argument("oper")
+
+parser.add_argument("--verbose", "-v", 
+                    action='store_true', 
+                    help="подробность вывода")
+parser.add_argument("--file", "-f", nargs = '?', type=argparse.FileType('w', encoding='utf-8'), 
+                    help="Запись в файл и вывод на экран")
+parser.add_argument("--quite", "-q", nargs = '?', type=argparse.FileType('w', encoding='utf-8'),
+                    help="Запись в файл без вывода")
+parser.add_argument("--append", "-a", nargs = '?', type=argparse.FileType('a', encoding='utf-8'),
+                    help="Дополнение в файл без вывода")
+args = parser.parse_args()
+print(args)
+def calc():
+    if args.oper == '+':
+        summa = 0
+        for i in range(len(args.nums)):
+            summa += args.nums[i]
+        return summa
+    
+    elif args.oper == '-':
+        diff = args.nums[0]
+        for i in range(len(args.nums)):
+            if i == 0:
+              continue
+            else:
+                diff -= args.nums[i]
+        return diff
+    
+    elif args.oper == '*':
+        multiple = args.nums[0]
+        for i in range(len(args.nums)):
+            if i == 0:
+                continue
+            else:
+                multiple *= args.nums[i]
+        return multiple
+    elif args.oper == '/':
+        dell = args.nums[0]
+        for i in range(len(args.nums)):
+            if i == 0:
+                continue
+            else:
+                dell /= args.nums[i]
+        return dell
+
+result = f' {args.oper} '.join(map(str, args.nums)) + ' = ' + str(calc())
+if args.verbose:
+    print(result)
+
+elif args.file:
+    args.file.write(result)
+    print(result)
+
+elif args.quite:
+    args.quite.write(result)
+
+elif args.append:
+    args.append.write(f'{result}\n')
+
+elif args.quite == None:
+    print('')
+
+
+
