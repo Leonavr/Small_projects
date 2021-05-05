9b2e2f84c8fd3f288d91bd8a4cb3284c3e98d0d0
Leonid Lypovyk <leonavr99@gmail.com>
2021-05-05 13:45:53 +0300

    fixed

--- Argparse_calc/calc1.py
+++ Argparse_calc/calc1.py
@@ -6,11 +6,11 @@ nums = parser.add_argument('nums', type=int, nargs='+',
 oper = parser.add_argument("oper")
 
 parser.add_argument("--verbose", "-v", 
-                    action='store_true', 
+                    action = 'store_true',
                     help="подробность вывода")
 parser.add_argument("--file", "-f", type=argparse.FileType('w', encoding='utf-8'), 
                     help="Запись в файл и вывод на экран")
-parser.add_argument("--quite", "-q", nargs = '?', type=argparse.FileType('w', encoding='utf-8'), 
+parser.add_argument("--quite", "-q", nargs = '?', const=True, action="store",
                     help="Запись в файл без вывода")
 parser.add_argument("--append", "-a", type=argparse.FileType('a', encoding='utf-8'),
                     help="Дополнение в файл без вывода")
@@ -48,40 +48,24 @@ def calc():
             else:
                 dell /= args.nums[i]
         return dell
-print(args)
+
 result = f' {args.oper} '.join(map(str, args.nums)) + ' = ' + str(calc())
 
-if args.verbose == True and args.file == None and args.append == None and args.quite == None:
+if args.verbose:
     print(result)
 
-if args.verbose == False and args.file and args.append == None and args.quite == None:
-    args.file.write(f'{result}\n')
-    print(result)
-
-if args.verbose == False and args.file == None and args.append and args.quite == None:
-    args.append.write(f'{result}\n')
-
-if args.verbose == False and args.file == None and args.append == None and args.quite:
-    args.quite.write(f'{result}\n')
-
-if args.verbose == False and args.file == None and args.append == None and args.quite == None:
-    print('')
-else:
-    print(calc())
-'''if args.verbose:
-    print(result)
 elif args.file:
     args.file.write(f'{result}\n')
     print(result)
 
 elif args.quite:
-    args.quite.write(f'{result}\n')
+    if not True:
+        with open (f'{args.quite}', 'w') as f:
+            f.write(result)
 
 elif args.append:
     args.append.write(f'{result}\n')
 
-elif args.quite == '-q':
-    print('')
-
 else:
-    print(calc())'''
+    print(calc())
+
